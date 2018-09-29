a1e0e77909ce2f910113d248a17b0a014edb2987
GYLee1618 <33380668+GYLee1618@users.noreply.github.com>
2018-09-28 22:56:33 -0400

    fixed error handler

--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ import random
 import math
 
 def status_parser(status):
+	try:
+		tokens = status[0]
+	except (IndexError):
+		return
 	tokens = status.split(' ')
 	# print(tokens)
 	data = {}
@@ -27,44 +31,50 @@ def status_parser(status):
 	return data
 
 def config_parser(config):
-    tokens = config[0]
-    #print (type(tokens))
-    tokens = tokens.split(' ')
-    data = {}
-    data['MAP_WIDTH'] = float(tokens[2])
-    data['MAP_HEIGHT'] = float(tokens[4])
-    data['CAPTURERADIUS'] = float(tokens[6])
-    data['VISIONRADIUS'] = float(tokens[8])
-    data['FRICTION'] = float(tokens[10])
-    data['BRAKEFRICTION'] = float(tokens[12])
-    data['BOMBPLACERADIUS'] = float(tokens[14])
-    data['BOMBEFFECTRADIUS'] = float(tokens[16])
-    data['BOMBDELAY'] = float(tokens[18])
-    data['BOMBPOWER'] = float(tokens[20])
-    data['SCANRADIUS'] = float(tokens[22])
-    data['SCANDELAY'] = float(tokens[24])
-    return data
-    
-def scan_parser(scan_data):
-    tokens = scan_data[0]
-    tokens = tokens.split(' ')
-    data = {}
-    num_mines = int(tokens[3])
-    data['mines'] = [tokens[3+3*i+1:3+3*i+4] for i in range(num_mines)]
-    pinfo_start = 3+3*num_mines+2
-    num_players = int(tokens[pinfo_start])
+	try:
+		tokens = config[0]
+	except (IndexError):
+		return
+	#print (type(tokens))
+	tokens = tokens.split(' ')
+	data = {}
+	data['MAP_WIDTH'] = float(tokens[2])
+	data['MAP_HEIGHT'] = float(tokens[4])
+	data['CAPTURERADIUS'] = float(tokens[6])
+	data['VISIONRADIUS'] = float(tokens[8])
+	data['FRICTION'] = float(tokens[10])
+	data['BRAKEFRICTION'] = float(tokens[12])
+	data['BOMBPLACERADIUS'] = float(tokens[14])
+	data['BOMBEFFECTRADIUS'] = float(tokens[16])
+	data['BOMBDELAY'] = float(tokens[18])
+	data['BOMBPOWER'] = float(tokens[20])
+	data['SCANRADIUS'] = float(tokens[22])
+	data['SCANDELAY'] = float(tokens[24])
+	return data
 
-    data['num_players'] = num_players
-    data['players'] = [tokens[pinfo_start+4*i+1:pinfo_start+4*i+5] for i in range(num_players)]
-    binfo_start = pinfo_start+4*num_players+2
-    num_bombs = int(tokens[binfo_start])
-    data['num_bombs'] = num_bombs
-    data['bombs'] = [tokens[binfo_start+3*i+1:binfo_start+3*i+4] for i in range(num_bombs)]
-    winfo_start = binfo_start+3*num_bombs+2
-    num_wormholes = int(tokens[winfo_start])
-    data['num_wormholes'] = num_wormholes
-    data['wormholes'] = [tokens[winfo_start+5*i+1:winfo_start+5*i+6] for i in range(num_wormholes)]
-    return data
+def scan_parser(scan_data):
+	try:
+		tokens = scan_data[0]
+	except (IndexError):
+		return
+	tokens = tokens.split(' ')
+	data = {}
+	num_mines = int(tokens[3])
+	data['mines'] = [tokens[3+3*i+1:3+3*i+4] for i in range(num_mines)]
+	pinfo_start = 3+3*num_mines+2
+	num_players = int(tokens[pinfo_start])
+
+	data['num_players'] = num_players
+	data['players'] = [tokens[pinfo_start+4*i+1:pinfo_start+4*i+5] for i in range(num_players)]
+	binfo_start = pinfo_start+4*num_players+2
+	num_bombs = int(tokens[binfo_start])
+	data['num_bombs'] = num_bombs
+	data['bombs'] = [tokens[binfo_start+3*i+1:binfo_start+3*i+4] for i in range(num_bombs)]
+	winfo_start = binfo_start+3*num_bombs+2
+	num_wormholes = int(tokens[winfo_start])
+	data['num_wormholes'] = num_wormholes
+	data['wormholes'] = [tokens[winfo_start+5*i+1:winfo_start+5*i+6] for i in range(num_wormholes)]
+	return data
 
 def status(user,password):
 	try:
@@ -74,30 +84,30 @@ def status(user,password):
 		return
 
 def move(user, password, angle, velocity):
-    angle = str(angle)
-    velocity = str(velocity)
-    run(user,password,'ACCELERATE '+angle+' '+velocity)
-    return 
+	angle = str(angle)
+	velocity = str(velocity)
+	run(user,password,'ACCELERATE '+angle+' '+velocity)
+	return 
 
 def brake(user, password):
-    run(user, password,'BRAKE')
-    return 
+	run(user, password,'BRAKE')
+	return 
 
 def scan(user, password, data, dic):
-    user_x = data['position'][0] 
-    user_y = data['position'][1] 
-    randx = random.randint(0,dic['MAP_WIDTH']) 
-    randy = random.randint(0,dic['MAP_WIDTH']) 
-    
-    scan_x = str(math.floor((float(user_x) + randx) % dic['MAP_WIDTH']))
-    scan_y = str(math.floor((float(user_y) + randy) % dic['MAP_WIDTH']))
-    print(scan_x, scan_y)
-    return get_scan(user, password, scan_x, scan_y)
+	user_x = data['position'][0] 
+	user_y = data['position'][1] 
+	randx = random.randint(0,dic['MAP_WIDTH']) 
+	randy = random.randint(0,dic['MAP_WIDTH']) 
+	
+	scan_x = str(math.floor((float(user_x) + randx) % dic['MAP_WIDTH']))
+	scan_y = str(math.floor((float(user_y) + randy) % dic['MAP_WIDTH']))
+	print(scan_x, scan_y)
+	return get_scan(user, password, scan_x, scan_y)
 
 if __name__ == '__main__':
 	u = 'BSOD'
 	p = 'Alboucai'
 	while True:
-            move(u,p,3.14/2,0.25+0.25)
-            print(status(u,p))
-            time.sleep(2)
+			move(u,p,3.14/2,0.25+0.25)
+			print(status(u,p))
+			time.sleep(2)
