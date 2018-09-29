f5ba67714dd09350b20260e482815e7318a5ea30
GYLee1618 <33380668+GYLee1618@users.noreply.github.com>
2018-09-29 05:06:21 -0400

    cleaned up, improved precision

--- get_path.py
+++ get_path.py
@@ -1,23 +1,24 @@
 import numpy as np
 from utils import *
 import pdb
+import math
 
 def min_dist(x0,y0,x1,y1,dimx,dimy):
 	points = [(x1+dimx*i,y1+dimy*j) for i in [-1,0,1] for j in [-1,0,1]]
 	min_dist = -1
 	for point in points:
-		dist = np.sqrt((x0-point[0])**2+(y0-point[1])**2)
+		dist = math.sqrt((x0-point[0])**2+(y0-point[1])**2)
 		if (min_dist < 0 or dist < min_dist):
 			min_dist = dist
 	return min_dist
 
 def get_path(x0,y0,x,y,dx,dy,dimx,dimy,braking):
 	# ideal_vec = np.array([x-x0,y-y0])
-	velocity =	 0#np.array([dx,dy])
+	velocity =	 0 #np.array([dx,dy])
 	points = [(x+dimx*i,y+dimy*j) for i in [-1,0,1] for j in [-1,0,1]]
 	min_dist = -1
 	for point in points:
-		dist = np.sqrt((x0-point[0])**2+(y0-point[1])**2)
+		dist = (x0-point[0])**2+(y0-point[1])**2
 		if (min_dist < 0 or dist < min_dist):
 			min_dist = dist
 			min_point = point
@@ -27,11 +28,13 @@ def get_path(x0,y0,x,y,dx,dy,dimx,dimy,braking):
 	ideal_vec = np.array([x-x0,y-y0])
 
 	fin_vel = (ideal_vec - velocity)
-
-	return min(1,np.linalg.norm(fin_vel)),np.sign(fin_vel[0])*np.arccos(fin_vel[0]/(np.linalg.norm(fin_vel)+.00001))
+	# print(math.acos((x-x0)/(math.sqrt((x-x0)**2+(y-y0)**2))))
+	if ((math.sqrt((x-x0)**2+(y-y0)**2)) == 0):
+		return 0,0
+	return min(1,math.sqrt((x-x0)**2+(y-y0)**2)),np.sign(x-x0)*math.acos((x-x0)/(math.sqrt((x-x0)**2+(y-y0)**2)))
 
 def exec_move(user,password,x,y):
-	while (np.linalg.norm(np.array(status(user,password)['velocity'])) > 5) :
+	while (np.linalg.norm(np.array(status(user,password)['velocity'])) > 1) :
 		brake(user,password)
 	config = None
 	braking = False
@@ -45,31 +48,18 @@ def exec_move(user,password,x,y):
 		else:
 			pos = stats['position']
 			vel = stats['velocity']
-			wormholes = []
-			avoid_worm = False
-			for wormhole in wormholes:
-				wx = float(wormhole[0])
-				wy = float(wormhole[1])
-				r = float(wormhole[2])
-				if (min_dist(float(pos[0]),float(pos[1]),wx,wy,float(config['MAP_WIDTH']),float(config['MAP_HEIGHT']))) < 10*r:
-					accel_x_y = (wy-y,-wx+x)
-					accel = (1,np.sign(wy-y)*np.arccos((wy-y)/(np.sqrt((wy-y)**2+(-wx+x)**2)+.001)))
-					avoid_worm = True
-					break
 			
-			if not avoid_worm:
-				accel = get_path(float(pos[0]),float(pos[1]),x,y,float(vel[0]),float(vel[1]),float(config['MAP_WIDTH']),float(config['MAP_HEIGHT']),braking)
+			accel = get_path(float(pos[0]),float(pos[1]),x,y,float(vel[0]),float(vel[1]),float(config['MAP_WIDTH']),float(config['MAP_HEIGHT']),braking)
 			# print(stats['position'],accel[0])
 			move(user,password,accel[1],accel[0])
-			if (min_dist(x,y,float(pos[0]),float(pos[1]),float(config['MAP_WIDTH']),float(config['MAP_HEIGHT'])) < 200 and not braking):
-				while (np.linalg.norm(np.array(status(user,password)['velocity'])) > .005):
+			if (min_dist(x,y,float(pos[0]),float(pos[1]),float(config['MAP_WIDTH']),float(config['MAP_HEIGHT'])) < 250 and not braking):
+				while (np.linalg.norm(np.array(status(user,password)['velocity'])) > .01):
 					brake(user,password)
-				braking	= True	
+				braking	= True
 
-			if (min_dist(x,y,float(pos[0]),float(pos[1]),float(config['MAP_WIDTH']),float(config['MAP_HEIGHT'])) < float(config['CAPTURERADIUS']) + 5):
+			if (min_dist(x,y,float(pos[0]),float(pos[1]),float(config['MAP_WIDTH']),float(config['MAP_HEIGHT'])) < config['CAPTURERADIUS'] + 5):
 				break
 	
 
 if __name__ == '__main__':
-	exec_move('BSOD','Alboucai',500,500)
-	exec_move('BSOD','Alboucai',5000,5000)
+	exec_move('BSOD','Alboucai',6000,6000)
