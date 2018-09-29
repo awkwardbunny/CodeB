669d83bbe253976fd86a3201ef770f5b609f8c40
Casey He <casey.he@gmail.com>
2018-09-28 20:05:58 -0400

    added mina test

--- test.py
+++ test.py
@@ -5,18 +5,35 @@ import time
 u = 'BSOD'
 pw = 'Alboucai'
 
-#run(u, pw,'BRAKE')
-num = 3.14 *1/3
 
 config = get_config(u,pw)
 c_dic = utils.config_parser(config)
 
+#initialize the dictionaries
+mine_db = {}
+worm_db = {}
+
 while True:
     status = get_status(u,pw)
     data = utils.status_parser(status[0])
-    pos = data['position']
+    if data['wormholes']:
+        worm_x, worm_y = data['wormholes'][0][1], data['wormholes'][0][2]
+        worm_db[worm_x+worm_y] = (worm_x, worm_y)
+        print(worm_db[worm_x+worm_y], 'worm')
+    if data['mines']:
+        mine_x, mine_y = data['mines'][0][1], data['mines'][0][2]
+        mine_db[mine_x+mine_y] = (mine_x, mine_y)
+        print(mine_db[mine_x+mine_y], 'mine')
     scan_data = utils.scan(u,pw,data,c_dic) 
     output = utils.scan_parser(scan_data)
-    print(scan_data)
-    print(output)
-    time.sleep(5.1)
+    if output == 0:
+        continue
+    
+    '''
+    if output['wormholes']:
+        worm_x, worm_y = output['wormholes'][0], output['wormholes'][1]
+        worm_db[worm_x+worm_y] = (worm_x, worm_y)   
+    if output['mines']:
+        mine_x, mine_y = output['mines'][0], output['mines'][1]
+        mine_db[mine_x+mine_y] = (mine_x, mine_y)
+    '''  
